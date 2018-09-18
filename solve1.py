2d2123f138081831c8b1b3b5755a8c5b0cc19532
Atikur Rahman <41461553+AtikurRahman05@users.noreply.github.com>
2018-09-18 11:45:06 +0600

    Create solve1.py

---
+++ solve1.py
@@ -0,0 +1,37 @@
+# Problem-solving
+class User:
+    name = ''
+    email=''
+    password=''
+    login = False
+
+    def __init__(self,name,email,password):
+        self.name = name
+        self.email = email
+        self.passward = passward
+    def login(self):
+        email = input("Enter email: ")
+        passward = input("Enter passward: ")
+
+        if email == self.email and passward ==self.passward:
+            login = True
+            print("Login Successfull")
+        else:
+                print("Login Fail")
+    def logout(self):
+        login = False
+        print("Logged out")
+    def isloggedin(self):
+        if self.login:
+            return True
+        else: 
+            return False
+    def profle(self):
+      if self.isloggedin():
+        print (self.name,"\n",self.email)
+      else:
+          print ("User is not logged in")
+User= User("Atikur","atikurra00005@gmail.com","12345") 
+User.login()
+User = input()       
+        
