e9efc81a393feef80895ecff5f8a6de8c109d5ee
Atikur Rahman <41461553+AtikurRahman05@users.noreply.github.com>
2019-08-06 20:39:13 +0600

    Create vowel.py
    Input a fruit name , if fruit name start with vowel print “this is an fruit_name” else, this is a fruit_name

---
+++ vowel.py
@@ -0,0 +1,11 @@
+# Problem-solving
+'''Input a fruit name , if fruit name start with vowel print “this is an fruit_name” else, this is a fruit_name
+'''
+a= input("enter a fruits name : ")
+x=a[0]
+for y in x:
+    if y in 'a,e,i,o,o':
+        print('this ia an fruit_name')
+    else:
+        print('this is a fruit_name')
+ 
